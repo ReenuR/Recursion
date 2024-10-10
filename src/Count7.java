d35ed4b07de2f622940e84b072c22f97e788210b
ReenuR <82420557+ReenuR@users.noreply.github.com>
2024-10-10 14:54:39 -0700

    Initial commit

---
+++ src/Count7.java
@@ -0,0 +1,15 @@
+public class Count7 {
+    public static void main(String[] args) {
+        System.out.println(count7(717));
+    }
+    public static  int count7(int n) {
+        if(n == 0)
+            return 0;
+
+        if(n % 10 == 7)
+            return 1 + count7(n / 10);
+
+        return count7(n / 10);
+
+    }
+}
